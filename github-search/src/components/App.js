7afd316090593d7d47d5ee560f23ca267445dfe0
Developer Tron <84941942+Dev-Tron@users.noreply.github.com>
2022-04-27 02:45:53 -0400

    add files

---
+++ github-search/src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+
+      </header>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
